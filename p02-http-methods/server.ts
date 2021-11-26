869b7b8bd05767d2100a3db56eb7a9b15ca3dbd4
Mariana <marianaufc@gmail.com>
2021-11-26 16:07:23 -0300

    creates crud

--- p02-http-methods/server.ts
+++ p02-http-methods/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './src/routes/index';
+import bodyParser from 'body-parser';
 
 const port = 3000;
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 app.use(routes);
 
 app.listen(port, () => {
--- p02-http-methods/src/routes/students.routes.ts
+++ p02-http-methods/src/routes/students.routes.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import { request } from 'http';
 import { Student } from '../models/student.model';
 
 const router = Router();
@@ -10,7 +11,7 @@ const students: Array<Student> = [
     { id: 4, name: 'Ana', email: 'ana@gmail.com', password: '123456', fee: 280, inclusion: new Date() }
 ];
 
-// GET
+// GET -------------------------------------------------------------------------------------
 router.get('/', (req: Request, res: Response) => {
     res.status(200).send(students);
 });
@@ -24,6 +25,39 @@ router.get('/id/:id', (req: Request, res: Response) => {
     }
 });
 
-// POST
+// POST  ------------------------------------------------------------------------------------
+router.post('/create', (req: Request, res: Response) => {
+    const nextId = (students[(students.length - 1)].id + 1);
+    const student = req.body;
+    student.id = nextId;
+    student.inclusion = new Date();
+    students.push(student);
+    res.status(201).send(student);
+});
+
+// PUT -------------------------------------------------------------------------------------
+router.put('update/:id', (req: Request, res: Response) => {
+    const partialStudent = req.body;
+    const student = students.find(student => student.id === parseInt(req.params.id));
+    if(!student) {
+        res.status(404).send({ message: 'Estudante não encontrado' });
+    } else {
+        student.name = partialStudent.name;
+        student.email = partialStudent.email;
+        res.status(200).send('');
+    }
+    console.log(partialStudent);    
+});
+
+// DELETE ----------------------------------------------------------------------------------
+router.delete('/remove/:id', (req: Request, res: Response) => {
+    const id = req.params.id;
+    const studentIndex = students.findIndex(student => student.id === parseInt(req.params.id));
+    if (studentIndex === -1) {
+        res.status(404).send({ message: 'Estudante não encontrado' });
+    } else {
+        students.splice(studentIndex, 1);
+    }
+});
 
 export default router;
